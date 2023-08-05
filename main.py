a6939c9cdc6a6e2deb8fd0355206d9f6982ea972
Seyyid Doğan <seyahdoo@gmail.com>
2023-08-06 00:19:00 +0100

    refactor

--- main.py
+++ main.py
@@ -28,14 +28,16 @@ Version: v1.0.1
     return
 
 def do_loop():
+    print("trying to locate the listen in button")
     while True:
         try:
             time.sleep(1)
             x, y = pyautogui.locateCenterOnScreen(PNG_PATH)
-            print(f"clicking listen in button on ({x}, {y})")
+            print(f"clicking on listen in button on ({x}, {y})")
             pyautogui.click(x, y)
-        except FileNotFoundError as e: print(e)
-        except Exception as e: pass
+        except FileNotFoundError as e: print(e); exit(1)
+        except TypeError as e: pass
+        except Exception as e: print(e); exit(1)
 
 if __name__ == "__main__":
     main()
