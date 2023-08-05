37bc8a030addbbff3a0ab85651b41b43a61c2bb5
Seyyid Doğan <seyahdoo@gmail.com>
2023-08-05 19:10:16 +0100

    still not building

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Author: Seyyid Ahmed Doğan (seyahdoo, seyahdoo@gmail.com)
 # Initial Release Date: 05/08/2023
-
+# Version: 1.0
 
 import pyautogui
 import time
-from version import version
 import os
+
+version = "1.0"
 
 print(f"Baldurs Gate 3 Auto Join Conversations v{version}")
 print(f"Author: Seyyid Ahmed Doğan (seyahdoo, seyahdoo@gmail.com)")
