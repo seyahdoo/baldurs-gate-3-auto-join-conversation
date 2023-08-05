6d20f8e7678fb7ed8f3ff5d1b12d29fbc50e7f2f
Seyyid Doğan <seyahdoo@gmail.com>
2023-08-05 23:58:43 +0100

    refactor

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 # Author: Seyyid Ahmed Doğan (seyahdoo, seyahdoo@gmail.com)
 # Initial Release Date: 05/08/2023
-# Version: 1.0
 
 import pyautogui
 import time
-import os
 
-version = "1.0"
+PNG_PATH = "listen-in.png"
+VERSION = "1.0.1"
 
-print(f"Baldurs Gate 3 Auto Join Conversations v{version}")
-print(f"Author: Seyyid Ahmed Doğan (seyahdoo, seyahdoo@gmail.com)")
-print(f"Initial Release Date: 05/08/2023")
+def main():
+    print_intro()    
+    do_loop()
 
-png_path = "listen-in.png"
+def print_intro():
+    print(f"""
+######################################################
+######################################################
+####                                              ####
+####    Baldurs Gate 3 Auto Join Conversations    ####
+####                                              ####
+######################################################
+######################################################
 
-while True:
-    try:
-        time.sleep(1)
-        location = pyautogui.locateOnScreen(png_path)
-        if(location == None):
-            continue
-        print(f"found listen in button {location}")
-        x = location.left + location.width/2
-        y = location.top + location.height/2
-        print(f"clicking listen in button on ({x}, {y})")
-        pyautogui.click(x, y)
-    except Exception as e:
-        print(e)
+Author: Seyyid Ahmed Doğan (seyahdoo, seyahdoo@gmail.com)
+Release Date: 05/08/2023
+Version: v1.0.1
+        """)
+    return
 
+def do_loop():
+    while True:
+        try:
+            time.sleep(1)
+            x, y = pyautogui.locateCenterOnScreen(PNG_PATH)
+            print(f"clicking listen in button on ({x}, {y})")
+            pyautogui.click(x, y)
+        except FileNotFoundError as e: print(e)
+        except Exception as e: pass
 
+if __name__ == "__main__":
+    main()
